14d3016f02f89cf3bb87775bda7c2ef95008c0fd
rasslen01 <160419199+rasslen01@users.noreply.github.com>
2025-08-23 16:31:42 +0200

    Update db.js

--- config/db.js
+++ config/db.js
@@ -2,8 +2,9 @@ const mongoose = require('mongoose');
 module.exports.connectToMongoDB = async () => {
 
 mongoose.set('strictQuery', false);
-mongoose.connect("mongodb+srv://sousse:g4tXzjquiBeGmzoo@cluster0.8s5fgp6.mongodb.net/").then(()=> {console.log('connected to MongoDB')}).catch((err)=>{console.log('Error connecting to MongoDB:', err)});
+mongoose.connect(process.env.Url_DB).then(()=> {console.log('connected to MongoDB')}).catch((err)=>{console.log('Error connecting to MongoDB:', err)});
 
 }
 //g4tXzjquiBeGmzoo
-//mongodb+srv://sousse:g4tXzjquiBeGmzoo@cluster0.8s5fgp6.mongodb.net/
+//mongodb+srv://sousse:g4tXzjquiBeGmzoo@cluster0.8s5fgp6.mongodb.net/   
+//mongodb+srv://sousse:<db_password>@cluster0.8s5fgp6.mongodb.net/        
